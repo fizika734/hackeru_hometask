76435f30996ffdb5c12054021a5d97c75c472f01
gggg <aleksey.chugunkov@glowbyteconsulting.com>
2020-11-08 23:22:13 +0300

    added description to monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -11,3 +11,17 @@ echo "Memory Usage: $mem_used MB of $mem_total MB"
 echo "Processes: $processes"
 echo $load_average
 echo "Logged users: $users"
+
+# there are 2 ways to run this script every 30 mins
+# first one is put line "30 * * * * PATH_TO_SCRIPT" in to cron (crontab -e) 
+# second one is to create a systemd unit with lines below
+# [Unit]
+# Description=Run scr
+
+# [Service]
+# User=%I
+# Restart=always
+# RestartSec=1800s
+# ExecStart=/bin/bash PATH_TO_SCRIPT
+# then you should run "sudo systemctl daemon-reload && sudo systemctl enable NAME_OF_UNIT_FILE"
+# Also you can trigger a script by systemd timer
